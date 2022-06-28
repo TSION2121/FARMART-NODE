e5b3adc97f9eaa1f9d236f13b329cdde6ec3dd43
TSION2121 <tsionbizuayehu3@gmail.com>
2022-06-28 23:42:12 +0300

    farmart

---
+++ config/db_Authentication.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv').config();
+
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+// const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_DATABASE = process.env.DB_DATABASE
+
+module.exports = {
+	dbAuth: {
+		host: DB_HOST,
+		user: DB_USER,
+		password: "",
+		database: DB_DATABASE,
+		multipleStatements: true,
+
+	}
+};
